9fffdba362f84e0015e2adf8902a8b98b5bd2214
sallarrezaie <91070355+sallarrezaie@users.noreply.github.com>
2022-12-20 21:53:44 +0700

    Terms and PP

--- backend/home/api/v1/urls.py
+++ backend/home/api/v1/urls.py
@@ -6,11 +6,16 @@ from home.api.v1.viewsets import (
     LoginViewSet,
 )
 from users.viewsets import UserViewSet
+from modules.terms_and_conditions.viewsets import TermAndConditionViewSet
+from modules.privacy_policy.viewsets import PrivacyPolicyViewSet
+
 
 router = DefaultRouter()
 router.register("signup", SignupViewSet, basename="signup")
 router.register("login", LoginViewSet, basename="login")
 router.register("users", UserViewSet, basename="users")
+router.register("privacy-policy", PrivacyPolicyViewSet, basename="privacy_policy_new")
+router.register("terms-and-conditions", TermAndConditionViewSet, basename="terms_and_conditions_new")
 
 urlpatterns = [
     path("", include(router.urls)),
